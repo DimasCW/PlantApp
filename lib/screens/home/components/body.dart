54ba25da9b7bdacfba8c777d50bfb50b039c1e1e
DimasCW <cdimas292@gmail.com>
2025-05-22 22:43:06 +0700

    menambahkan widget RecomandPlantCard di Body untuk menampilkan informasi tanaman rekomendasi, termasuk gambar, judul, negara, dan harga, meningkatkan tampilan dan interaksi pengguna

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -22,6 +22,13 @@ class Body extends StatelessWidget {
                   title: "Recommended",
                   press: () {},
                 ),
+                RecomandPlantCard(
+                  image: "assets/images/image_1.png",
+                  title: "Samantah",
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
                 Positioned(
                   bottom: 0,
                   left: 0,
@@ -66,6 +73,80 @@ class Body extends StatelessWidget {
           ),
           // You can continue adding more widgets here
         ],
+      ),
+    );
+  }
+}
+
+class RecomandPlantCard extends StatelessWidget {
+  const RecomandPlantCard({
+    super.key,
+    this.image,
+    this.title,
+    this.country,
+    this.price,
+    this.press,
+  });
+
+  final String image, title, country;
+  final int price;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding/2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding/2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  )
+                ]
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(children: [
+                      TextSpan(text: "$title\n".toUpperCase(),
+                      style: Theme.of(context).textTheme.button),
+                      TextSpan(text: "$country".toUpperCase(),
+                      style: TextStyle(
+                        color: kPrimaryColor.withOpacity(0.5),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )
+                      )
+                    ])
+                  ),
+                  Spacer(),
+                  Text('\$$price',
+                  style: Theme.of(context).textTheme.button.copyWith(color: kPrimaryColor),
+                  )
+                ]
+              ),
+            ),
+          ),
+        ]
       ),
     );
   }
