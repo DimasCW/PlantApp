8a1f8857df62ba0f99db8b71cc78f7458efdecb2
DimasCW <cdimas292@gmail.com>
2025-05-22 22:07:45 +0700

    mengganti struktur widget di Body dengan menambahkan HeaderWithSearchBox untuk meningkatkan keterbacaan dan pemisahan logika, serta memperbarui tampilan pencarian

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -9,66 +9,94 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.2,
+            child: Stack(
+              children: <Widget>[
+                HeaderWithSearchBox(size: size),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    height: 54,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 10),
+                          blurRadius: 50,
+                          color: kPrimaryColor.withOpacity(0.23),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            onChanged: (value) {},
+                            decoration: InputDecoration(
+                              hintText: "Search",
+                              hintStyle: TextStyle(
+                                color: kPrimaryColor.withOpacity(0.5),
+                              ),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
                             ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
                           ),
                         ),
-                      ),
-                      SvgPicture.asset("assets/icons/search.svg"),
-                    ],
+                        SvgPicture.asset("assets/icons/search.svg"),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          // You can continue adding more widgets here
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: kDefaultPadding,
+        right: kDefaultPadding,
+        bottom: 36 + kDefaultPadding,
+      ),
+      height: size.height * 0.2 - 27,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(36),
+          bottomRight: Radius.circular(36),
         ),
-        // You can continue adding more widgets here
-      ],
+      ),
+      child: Row(
+        children: <Widget>[
+          Text("Hi User", style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.white, fontWeight: FontWeight.bold),),
+          Spacer(),
+          Image.asset("assets/images/img.png"),
+        ],
+      ),
     );
   }
 }
